f46da006ec66f90ed9799bda074945880ac4a4e8
yangjing <yangjingpuyu@aliyun.com>
2018-07-20 23:59:25 +0800

    node server

---
+++ todo-ssr/server/routers/dev-ssr.js
@@ -0,0 +1,9 @@
+const Router = require('koa-router')
+const axios = require('axios')
+const MemoryFS = require('memory-fs')
+const webpack = require('webpack')
+const VueServerRenderer = require('vue-server-renderer')
+
+const serverConfig = require('../../build/webpack.config.server')
+
+const serverCompiler = webpack(serverConfig)
